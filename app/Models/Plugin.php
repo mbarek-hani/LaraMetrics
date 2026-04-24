9b52e7085bc0d930dca8ec9d9a8fc05f65c1d900
mbarek-hani <mbarekhani30@gmail.com>
2026-04-24 16:37:48 +0100

    update the Plugin model

--- app/Models/Plugin.php
+++ app/Models/Plugin.php
@@ -3,8 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\AsArray;
 
 class Plugin extends Model
 {
-    //
+    protected $fillable = [
+        "identifiant",
+        "nom",
+        "version",
+        "description",
+        "auteur",
+        "actif",
+        "installe",
+        "configuration",
+        "metadonnees",
+        "installe_le",
+        "active_le",
+    ];
+
+    protected $casts = [
+        "actif" => "boolean",
+        "installe" => "boolean",
+        "configuration" => AsArray::class,
+        "metadonnees" => AsArray::class,
+        "installe_le" => "datetime",
+        "active_le" => "datetime",
+    ];
+
+    public function scopeActifs($query)
+    {
+        return $query->where("actif", true);
+    }
+
+    public function scopeInstalles($query)
+    {
+        return $query->where("installe", true);
+    }
 }
