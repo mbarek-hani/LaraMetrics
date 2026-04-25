cd7b5361782899bdb1cf23ee0daabba74cf08873
mbarek-hani <mbarekhani30@gmail.com>
2026-04-25 18:24:01 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,21 +16,6 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
-        colors: {
-            primary: {
-                50:  '#f0fdf4',
-                100: '#dcfce7',
-                200: '#bbf7d0',
-                300: '#86efac',
-                400: '#4ade80',
-                500: '#22c55e',
-                600: '#16a34a',
-                700: '#15803d',
-                800: '#166534',
-                900: '#14532d',
-                950: '#052e16',
-            },
-        },
     },
 
     plugins: [forms],
