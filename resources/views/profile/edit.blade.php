115f49222a199623da1b6d73ceaef0b4e5eae6f0
mbarek-hani <mbarekhani30@gmail.com>
2026-04-29 17:18:38 +0100

    refactor

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,7 +7,6 @@
         <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 space-y-4">
             @include('profile.partials.update-profile-information-form')
             @include('profile.partials.update-password-form')
-            @include('profile.partials.delete-user-form')
         </div>
     </div>
 </x-app-layout>
