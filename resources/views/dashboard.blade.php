6026bfb3d098961ad0e81bc60e6e83e6a6f4ee74
mbarek-hani <mbarekhani30@gmail.com>
2026-04-29 18:25:20 +0100

    make the dashboard main content's width a little bit smaller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
 
     <div class="py-4">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
 
             @if($aucunSite)
                 <x-card>
