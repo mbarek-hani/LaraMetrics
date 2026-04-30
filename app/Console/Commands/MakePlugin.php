3c6ce6545b775cc42ef5e6c3a5024fcf66f85a27
mbarek-hani <mbarekhani30@gmail.com>
2026-04-30 19:49:58 +0100

    implements a command to scaffold a new plugin structure

---
+++ app/Console/Commands/MakePlugin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+
+class MakePlugin extends Command
+{
+    protected $signature = "make:plugin {name: The name of the plugin in PascalCase}";
+    protected $description = "Create a new LaraMetrics plugin structure.";
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = Str::studly(
+            $this->argument("name: The name of the plugin in PascalCase"),
+        );
+        $path = base_path("plugins/{$name}");
+
+        if (File::exists($path)) {
+            $this->error("The plugin {$name} already exists");
+            return;
+        }
+
+        $direcories = [
+            $path . "/src",
+            $path . "/routes",
+            $path . "/resources/views",
+            $path . "/database/migrations",
+        ];
+
+        foreach ($direcories as $dir) {
+            File::makeDirectory($dir, 0755, true);
+        }
+
+        $identifiant = Str::kebab($name);
+        File::put(
+            $path . "/manifest.json",
+            json_encode(
+                [
+                    "identifiant" => $identifiant,
+                    "nom" => $name,
+                    "version" => "1.0.0",
+                    "classe" => "Plugins\\{$name}\\{$name}",
+                    "description" => "Description pour {$name}",
+                ],
+                JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES,
+            ),
+        );
+
+        $stub = $this->getPluginStub($name, $identifiant);
+        File::put($path . "/src/{$name}.php", $stub);
+
+        File::put(
+            $path . "/routes/web.php",
+            "<?php\n\nuse Illuminate\\Support\\Facades\\Route;\n\n// Routes pour {$name}",
+        );
+
+        $this->info("Plugin {$name} created!");
+    }
+
+    protected function getPluginStub(string $name, string $id)
+    {
+        return "<?php\n\nnamespace Plugins\\{$name};\n\nuse App\Core\Plugin\AbstractPlugin;\n\nclass {$name} extends AbstractPlugin {\n    //\n}\n\n";
+    }
+}
