e31e1112444e36c862d78101144a23c8d6ef7b5b
mbarek-hani <mbarekhani30@gmail.com>
2026-05-01 20:43:28 +0100

    migrate the application logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,14 +1,11 @@
-<!-- resources/views/components/application-logo.blade.php -->
-<div {{ $attributes->merge(['class' => 'flex items-center gap-1']) }}>
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-10 h-10">
+<div {{ $attributes->merge(['class' => 'c-logo']) }}>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="c-logo__icon">
         <path d="M4 17L10 11L14 15L20 9" stroke="currentColor" stroke-width="3" stroke-linecap="round"
-            stroke-linejoin="round" class="text-emerald-500" />
-        <path d="M16 9H20V13" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
-            class="text-emerald-500" />
+            stroke-linejoin="round" />
+        <path d="M16 9H20V13" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
     </svg>
 
-    <!-- Texte FLUX -->
-    <span class="text-2xl font-black tracking-tighter text-emerald-500 uppercase select-none">
+    <span class="c-logo__text">
         Flux
     </span>
 </div>
