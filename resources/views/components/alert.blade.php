7ce813e23c8ec4d592669b4ead58530e0c05636e
mbarek-hani <mbarekhani30@gmail.com>
2026-05-01 20:40:10 +0100

    migrate the alert component to use vanilla css

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,26 +1,26 @@
 @props(['type' => 'succes', 'message' => ''])
 
 @php
-    $classes = [
-        'succes' => 'bg-emerald-50 border-emerald-200 text-emerald-700 border-l-4 border-l-emerald-500',
-        'erreur' => 'bg-red-50 border-red-200 text-red-700 border-l-4 border-l-red-500',
-    ][$type] ?? 'bg-blue-50 border-blue-200 text-blue-700 border-l-4 border-l-blue-500';
+    $modifier = match ($type) {
+        'succes' => 'c-alert--success',
+        'erreur' => 'c-alert--erreur',
+        default => 'c-alert--info',
+    };
 @endphp
 
 <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 5000)" x-show="show"
     x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform translate-x-8"
     x-transition:enter-end="opacity-100 transform translate-x-0" x-transition:leave="transition ease-in duration-300"
     x-transition:leave-start="opacity-100 transform translate-x-0"
-    x-transition:leave-end="opacity-0 transform translate-x-8" class="fixed top-4 right-4 z-[100] w-full max-w-sm"
-    x-cloak>
-    <div class="{{ $classes }} shadow-lg rounded-r p-4 flex items-start gap-3 border shadow-sm">
-        <div class="flex-1">
-            <p class="text-sm font-medium">
-                {{ $message }}
-            </p>
+    x-transition:leave-end="opacity-0 transform translate-x-8" class="c-alert-wrapper" x-cloak>
+
+    <div class="c-alert {{ $modifier }}">
+        <div class="c-alert__message">
+            <p>{{ $message }}</p>
         </div>
-        <button @click="show = false" class="text-current opacity-50 hover:opacity-100 transition">
-            <x-custom-icon name="x-mark" class="w-4 h-4" />
+
+        <button @click="show = false" class="c-alert__close">
+            <x-custom-icon name="x-mark" style="width: 1rem; height: 1rem;" />
         </button>
     </div>
 </div>
