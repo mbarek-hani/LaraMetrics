26cba60235a2ad92e0c81370dc94b83b4005eb7e
mbarek-hani <mbarekhani30@gmail.com>
2026-05-05 19:52:45 +0100

    migrate modal component

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -6,11 +6,11 @@
 
 @php
 $maxWidth = [
-    'sm' => 'sm:max-w-sm',
-    'md' => 'sm:max-w-md',
-    'lg' => 'sm:max-w-lg',
-    'xl' => 'sm:max-w-xl',
-    '2xl' => 'sm:max-w-2xl',
+    'sm' => 'c-modal__content--sm',
+    'md' => 'c-modal__content--md',
+    'lg' => 'c-modal__content--lg',
+    'xl' => 'c-modal__content--xl',
+    '2xl' => 'c-modal__content--2xl',
 ][$maxWidth];
 @endphp
 
@@ -33,10 +33,10 @@ $maxWidth = [
     }"
     x-init="$watch('show', value => {
         if (value) {
-            document.body.classList.add('overflow-y-hidden');
+            document.body.style.overflowY = 'hidden';
             {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
         } else {
-            document.body.classList.remove('overflow-y-hidden');
+            document.body.style.overflowY = '';
         }
     })"
     x-on:open-modal.window="$event.detail == '{{ $name }}' ? show = true : null"
@@ -46,32 +46,32 @@ $maxWidth = [
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
     x-show="show"
-    class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
+    class="c-modal"
     style="display: {{ $show ? 'block' : 'none' }};"
 >
     <div
         x-show="show"
-        class="fixed inset-0 transform transition-all"
+        class="c-modal__overlay"
         x-on:click="show = false"
-        x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
+        x-transition:enter="c-transition-modal-enter"
+        x-transition:enter-start="c-transition-modal-enter-start"
+        x-transition:enter-end="c-transition-modal-enter-end"
+        x-transition:leave="c-transition-modal-leave"
+        x-transition:leave-start="c-transition-modal-leave-start"
+        x-transition:leave-end="c-transition-modal-leave-end"
     >
-        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        <div class="c-modal__backdrop"></div>
     </div>
 
     <div
         x-show="show"
-        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
-        x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        class="c-modal__content {{ $maxWidth }}"
+        x-transition:enter="c-transition-modal-content-enter"
+        x-transition:enter-start="c-transition-modal-content-enter-start"
+        x-transition:enter-end="c-transition-modal-content-enter-end"
+        x-transition:leave="c-transition-modal-content-leave"
+        x-transition:leave-start="c-transition-modal-content-leave-start"
+        x-transition:leave-end="c-transition-modal-content-leave-end"
     >
         {{ $slot }}
     </div>
