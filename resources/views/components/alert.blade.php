2ff492bd33962fcf533b2afeff85ce3a864c8bea
mbarek-hani <mbarekhani30@gmail.com>
2026-05-06 18:55:10 +0100

    refactor: update alert component layout and migrate Alpine transitions to CSS classes

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -10,10 +10,13 @@
 @endphp
 
 <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 5000)" x-show="show"
-    x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform translate-x-8"
-    x-transition:enter-end="opacity-100 transform translate-x-0" x-transition:leave="transition ease-in duration-300"
-    x-transition:leave-start="opacity-100 transform translate-x-0"
-    x-transition:leave-end="opacity-0 transform translate-x-8" class="c-alert-wrapper" x-cloak>
+    x-transition:enter="c-alert-enter" 
+    x-transition:enter-start="c-alert-enter-start"
+    x-transition:enter-end="c-alert-enter-end" 
+    x-transition:leave="c-alert-leave"
+    x-transition:leave-start="c-alert-leave-start"
+    x-transition:leave-end="c-alert-leave-end" 
+    class="c-alert-wrapper" x-cloak>
 
     <div class="c-alert {{ $modifier }}">
         <div class="c-alert__message">
